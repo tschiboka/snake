e6243b79788085f7df9eefea4b2b6de32c67190c
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2020-03-28 23:28:39 +0000

    handle keypress

--- snake.js
+++ snake.js
@@ -1,3 +1,5 @@
+// HELPER FUNCTIONS
+// get DOM elements
 const $ = (selector, all = false) => all ? [...document.querySelectorAll(selector)] : document.querySelector(selector);
 
 const app = {
@@ -7,16 +9,21 @@ const app = {
     $gameBox: $(".game-box"),
     $control: $(".game-box__control"),
     // APP VARS
-    introDuration: 1,
-    loadLevelsState: "pending",
-    currentLevel: 1,
-    level: undefined,
-    keyboard: false,
-    gameTablePadding: 10,
-    gameTableCellLength: 20,
+    introDuration: 1,                                  // the intro animation in secs
+    loadLevelsState: "pending",                        // if level JSON is loaded (pending | success | error)
+    currentLevel: 1,                                   // default 1 unless local store has level stored
+    level: undefined,                                  // level object is set later  
+    keyboard: false,                                   // if player can interact with keyboard
+    gameTablePadding: 10,                              // the padding around the game arena in px
+    gameTableCellLength: 50,                           // the width and length of a single arena table cell in px
+    interactionAllowed: false,                         // if player can interact with the game
 };
 
-const level = {};
+// most values will be declared by different functions, but they are all declared for easier readability
+const arena = {
+    focusX: undefined,                                 // the snakes heads X pos
+    focusY: undefined,                                 // the snakes heads Y pos
+}
 
 
 
@@ -59,20 +66,35 @@ function setLevelNumFromLocalStorage() {
 
 
 
-function handleClickEvents(e) {
+function handleClick(e) {
     // delegate all click events on body
     const origin = e.target.getAttribute("data-event");
 
     switch (origin) {
-        case "start-btn": { startLevel(); }
+        case "start-btn": { startLevel(); break; }
     }
 }
 
 
 
 function handleResize() {
-    app.$gameBox.removeChild($(".game-table"));
-    buildTable();
+    app.$gameBox.removeChild($(".game-arena"));
+    buildGameArena();
+}
+
+
+
+function handleKeypress(e) {
+    if (app.interactionAllowed) {
+        switch (e.keyCode) {
+            case 38: { console.log("UP"); break; }
+            case 40: { console.log("DOWN"); break; }
+            case 37: { console.log("LEFT"); break; }
+            case 39: { console.log("RIGHT"); break; }
+            case 32: { console.log("SPACE"); break; }
+        }
+
+    }
 }
 
 
@@ -81,7 +103,8 @@ function startGame() {
     app.$intro.style.display = "none";
     app.$mainMenu.style.display = "flex";
 
-    $("body").addEventListener("click", e => handleClickEvents(e));
+    $("body").addEventListener("click", handleClick);
+    document.addEventListener("keydown", handleKeypress)
     window.addEventListener("resize", handleResize);
 }
 
@@ -90,18 +113,22 @@ function startGame() {
 function startLevel() {
     app.$mainMenu.style.display = "none";
     app.$gameBox.style.display = "flex";
+
     if (app.keyboard) { app.$control.style.display = "none"; }
-    console.log(app.$control);
 
     // assign current level to app obj
     app.level = app.levels[app.currentLevel - 1];
 
-    buildTable();
+    arena.focusX = app.level.focus[0];
+    arena.focusY = app.level.focus[1];
+    arena.objects = app.level.objects;
+
+    buildGameArena();
 }
 
 
 
-function buildTable() {
+function buildGameArena() {
     const height = window.innerHeight;
     const width = window.innerWidth;
     const gameHeight = height - (app.gameTablePadding * 2);
@@ -115,16 +142,39 @@ function buildTable() {
 
     $(".game-box__stats, .game-box__control", true).map(elem => elem.style.height = Math.floor(gameHeight / 10) + "px");
 
-    console.log(rowNum, colNum);
-    // build table
-    table.classList.add("game-table");
-    for (let row = 0; row < rowNum; row++) {
+    table.classList.add("game-arena");
+    for (let r = 0; r < rowNum; r++) {
         const row = document.createElement("tr");
-        for (let cell = 0; cell < colNum; cell++) {
+        for (let c = 0; c < colNum; c++) {
             const cell = document.createElement("td");
+            cell.id = `x${r}y${c}`
+            cell.innerHTML = r + "," + c;
             row.appendChild(cell);
         }
+        table.style.width = colNum * app.gameTableCellLength + "px";
+        table.style.height = rowNum * app.gameTableCellLength + "px";
         table.appendChild(row);
     }
     app.$gameBox.appendChild(table);
+
+    placeObjectsInArena();
+}
+
+
+
+function placeObjectsInArena() {
+    // temporary!!!!!!
+    $(`#x${arena.focusX}y${arena.focusY}`).style.backgroundColor = "green";
+
+
+    arena.objects.map(obj => {
+        switch (obj.type) {
+            case "wall": {
+
+                break;
+            }
+        }
+    });
+
+    app.interactionAllowed = true;
 }
