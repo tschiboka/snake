a5b471b290c1de200b8dca8aaa329d402c3197c9
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2020-03-30 10:45:52 +0100

    restart obj placing, find center of table

--- snake.js
+++ snake.js
@@ -24,7 +24,7 @@ const app = {
     loadLevelsState: "pending",                        // ("pending" | "success" | "error") -> if level JSON is loaded
     currentLevel: 1,                                   // (+int) -> default 1 unless local store has level stored
     level: undefined,                                  // (obj)  -> level object is loaded later  
-    keyboard: false,                                   // (flag) -> if player can interact with keyboard eg mobile
+    keyboard: true,                                    // (flag) -> if player can interact with keyboard eg mobile
     gameTablePadding: 10,                              // (+int) -> the padding around the game arena in px
     gameTableCellLength: 30,                           // (+int) -> the width and length of a single arena table cell in px
     interactionAllowed: false,                         // (flag) -> if player can interact with the game
@@ -36,12 +36,14 @@ const app = {
     Most values will be declared by different game-play functions, but they are all defined here for easier readability
     (easier to look up and organize properties, values and their type (eg num, str))*/
 const arena = {
-    focusX: undefined,                                 // (+int) -> the display areas center X pos, floored (coord) 
-    focusY: undefined,                                 // (+int) -> the display areas center Y pos, floored (coord)
+    //    focusX: undefined,                                 // (+int) -> the display areas center X pos, floored (coord) 
+    //    focusY: undefined,                                 // (+int) -> the display areas center Y pos, floored (coord)
+    //    crossHairX: undefined,                             // (+int) -> the x coord on the map where display is centered
+    //    crossHairY: undefined,                             // (+int) -> the x coord on the map where display is centered
     tableRowNum: undefined,                            // (+int) -> the displayable area expressed in table rows (coord)
     tableColNum: undefined,                            // (+int) -> the displayable area expressed in table columns (coord)
-    centerHead: true,                                  // (flag) -> the first display tries to center map around characters head (focus point)
-    objects: undefined,                                // (arr of objs) -> list of objects that appears on the game map
+    gameEntities: undefined,                           // (arr of objs) -> list of objects that appears on the game map
+    characterCoords: [],                               // 
 }
 
 
@@ -68,7 +70,8 @@ function handleClick(e) {
 // For responsivity games display area is scaling proportionally to windows available area
 function handleResize() {
     app.$gameBox.removeChild($(".game-arena"));
-    buildGameDisplayableArea();
+    buildGameTableArea();
+    arena.centerHead = true;
 }
 
 
@@ -78,10 +81,10 @@ function handleResize() {
 function handleKeypress(e) {
     if (app.interactionAllowed) {
         switch (e.keyCode) {
-            case 38: { console.log("UP"); moveFocusPoint(0, -1); break; }
-            case 40: { console.log("DOWN"); moveFocusPoint(0, 1); break; }
-            case 37: { console.log("LEFT"); moveFocusPoint(-1, 0); break; }
-            case 39: { console.log("RIGHT"); moveFocusPoint(1, 0); break; }
+            case 38: { console.log("UP"); moveCharacter(0, -1); break; }
+            case 40: { console.log("DOWN"); moveCharacter(0, 1); break; }
+            case 37: { console.log("LEFT"); moveCharacter(-1, 0); break; }
+            case 39: { console.log("RIGHT"); moveCharacter(1, 0); break; }
             case 32: { console.log("SPACE"); break; }
         }
 
@@ -161,23 +164,39 @@ function startLevel() {
     // assign current level to app obj
     app.level = app.levels[app.currentLevel - 1];
 
-    arena.objects = app.level.objects;
+    arena.gameEntities = createGameArenaObjectArr(app.level.objects);
 
-    buildGameDisplayableArea();
+    buildGameTableArea();
+}
+
+
+
+function createGameArenaObjectArr(obj) {
+    // create an emtpty 2D array
+    let entities = Array(app.level.dimension.rows).fill().map(() => Array(app.level.dimension.cols).fill());
+
+    // objects in the map area
+    obj.forEach(o => { entities[o.row][o.col] = o; });
+
+    // game character
+    arena.gameCharacterCoords = app.level.gameCharacterCoords;
+    arena.gameCharacterCoords.map(coord => entities[coord[0]][coord[1]] = coord);
+
+    return entities;
 }
 
 
 
 /*##################################################################################################
-  #####################################   GAME-PLAY FUNCTIONS  #####################################
+  ##############################   BUILD GAMETABLE & POSITIONING  ##################################
   ##################################################################################################*/
 
 
 
 // Build a table that displays the level map (or most cases a segment of it), and scale it to the 
 // space currently available in the browser window.
-function buildGameDisplayableArea() {
-    if (app.level.dimension[0] < 10 | app.level.dimension[1] < 10) throw new RangeError(`Level dimension must be min 10x10!\nCurrent dimension on level ${app.currentLevel} is ${app.level.dimension[0]}x${app.level.dimension[1]}.`)
+function buildGameTableArea() {
+    if (app.level.dimension.cols < 10 | app.level.dimension.rows < 10) throw new RangeError(`Level dimension must be min 10x10!\nCurrent dimension on level ${app.currentLevel} is ${app.level.dimension.cols}x${app.level.dimension.rows}.`)
     const height = window.innerHeight;
     const width = window.innerWidth;
     const gameHeight = height - (app.gameTablePadding * 2);
@@ -185,8 +204,8 @@ function buildGameDisplayableArea() {
     const navSpace = app.keyboard ? gameHeight * 0.1 : gameHeight * 0.2; // space for displaying points and navigation etc.
     const maxDisplayableRows = Math.floor((gameHeight - navSpace) / app.gameTableCellLength);
     const maxDisplayableCols = Math.floor(gameWidth / app.gameTableCellLength);
-    const rowNum = app.level.dimension[1] <= maxDisplayableRows ? app.level.dimension[1] : maxDisplayableRows;
-    const colNum = app.level.dimension[0] <= maxDisplayableCols ? app.level.dimension[0] : maxDisplayableCols;
+    const rowNum = app.level.dimension.rows <= maxDisplayableRows ? app.level.dimension.rows : maxDisplayableRows;
+    const colNum = app.level.dimension.cols <= maxDisplayableCols ? app.level.dimension.cols : maxDisplayableCols;
     const table = document.createElement("table");
 
     $(".game-box__stats, .game-box__control", true).map(elem => elem.style.height = Math.floor(gameHeight / 10) + "px");
@@ -208,78 +227,29 @@ function buildGameDisplayableArea() {
     arena.tableRowNum = rowNum;
     arena.tableColNum = colNum;
 
-    placeObjectsInArena();
+
+    placeTableAtMap();
 }
 
 
 
-// Display all objects provided by the actual level on the game table.
-function placeObjectsInArena() {
-    if (arena.centerHead) centerDisplayAreaToCenterCharacterHead();
-    // temporary!!!!!!
-    //$(`#r${arena.focusY}c${arena.focusX}`).style.backgroundColor = "green";
-
-    //$(`#r${getTableCenterCoords().y}c${getTableCenterCoords().x}`).style.backgroundColor = "rgba(255, 30, 0, 0.1)";
-
-    for (let r = 0; r < arena.tableRowNum; r++) {
-        for (let c = 0; c < arena.tableColNum; c++) {
-            try {
-
-                $(`#r${r}c${c}`).title = r + "," + c;
-            }
-            catch (err) { console.log(r, c); }
-        }
-    }
-
-    arena.objects.map(obj => {
-        switch (obj.type) {
-            case "wall": {
-
-                break;
-            }
-        }
-    });
-
-    app.interactionAllowed = true;
+function placeTableAtMap() {
+    centerTableAroundCharacter();
 }
 
 
+function centerTableAroundCharacter() {
+    const head = arena.gameCharacterCoords[0];
+    const centerRowOnTable = Math.floor((arena.tableRowNum - 1) / 2);
+    const centerColOnTable = Math.floor((arena.tableColNum - 1) / 2);
 
-// Head should be at the center of the display table at the begining of the level.
-// If head would be out of the range of level map, set focus as close to the edges as possible.
-// eg: focusX can't be -1 or larger than maps max column num. 
-function centerDisplayAreaToCenterCharacterHead() {
-    let arenaMapCrosshairX = app.level.characterHeadAt[1];
-    let arenaMapCrosshairY = app.level.characterHeadAt[0];
+    console.log(centerRowOnTable, centerColOnTable);
 
-    if (arenaMapCrosshairX < 0 || arenaMapCrosshairY < 0) throw new RangeError(`Variables below can not have negative values! \narenaMapCrosshairX: ${arenaMapCrosshairX}, arenaMapCrosshairY: ${arenaMapCrosshairY}`);
-
-    // check if display table is in range
-
-    getTableDistancePoints();
+    // temporalily paint head
+    $(`#r${head[0]}c${head[1]}`).style.background = "lightgreen";
+    $(`#r${centerRowOnTable}c${centerColOnTable}`).style.background = "rgba(255, 255, 255, 0.1)";
 }
 
+function moveCharacter(col, row) {
 
-
-
-// Returns an obj with the display tables center x, y coords (eg: 10x10 -> x:4, y:4, 11x11 -> x:5, y:5) 
-const getTableDistancePoints = () => {
-    const centerX = Math.floor((arena.tableColNum - 1) / 2);
-    const centerY = Math.floor((arena.tableRowNum - 1) / 2);
-    const distRight = arena.tableColNum - 1 - centerX;
-    const distLeft = arena.tableColNum - 1 - distRight;
-    const distBottom = arena.tableRowNum - 1 - centerY;
-    const distTop = arena.tableRowNum - 1 - distBottom;
-    console.log(centerX, centerY);
-    console.log(distLeft, distRight);
-    console.log(distTop, distBottom);
-}
-
-
-
-function moveFocusPoint(x, y) {
-    arena.focusX += x;
-    arena.focusY += y;
-
-    placeObjectsInArena();
 }
